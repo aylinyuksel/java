1a87c1f937c8ddc01f921e550d55a1d61d29931e
feyzatiryakii <f3yzatiryaki@gmail.com>
2024-12-30 12:23:10 +0300

    #37 hata kontrol

--- game/src/main/KeyHandler.java
+++ game/src/main/KeyHandler.java
@@ -259,8 +259,9 @@ public class KeyHandler implements KeyListener {
 	public void gameOverState(int code) {
 		
 		if(code == KeyEvent.VK_W) {
+			
 			gp.ui.commandNum--;
-			if(gp.ui.commandNum <= 0) {
+			if(gp.ui.commandNum < 0) {				
 				gp.ui.commandNum = 1; //1 - QUIT
 			}
 			
@@ -269,14 +270,18 @@ public class KeyHandler implements KeyListener {
 		
 		if(code == KeyEvent.VK_S) {
 			gp.ui.commandNum++;
-			if(gp.ui.commandNum > 1) {
+			if(gp.ui.commandNum> 1) {
 				gp.ui.commandNum = 0; //0 - RETRY
 			}
+			
+			/*if(commandNum == 0) {
+				g2.drawString(">", x-gp.tileSize, y);
+			}*/
 			
 			gp.playSE(9);
 		}
 		
-		if(code == KeyEvent.VK_ENTER) {
+		/*if(code == KeyEvent.VK_ENTER) {
 			if(gp.ui.commandNum == 0) {
 				gp.gameState = gp.playState;
 				gp.retry();
@@ -285,7 +290,7 @@ public class KeyHandler implements KeyListener {
 				gp.gameState = gp.titleState;
 				gp.restart();
 			}
-		}
+		}*/
 		
 		
 	}
