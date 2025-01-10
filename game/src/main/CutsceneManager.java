3b74dac3a6d91f56feaaab03c76e18566b0fa162
userayse404 <152299003+userayse404@users.noreply.github.com>
2025-01-10 21:59:20 +0300

    names

--- game/src/main/CutsceneManager.java
+++ game/src/main/CutsceneManager.java
@@ -1,3 +1,10 @@
+/*Ali Buğra Tekin 230610058
+Aylin Yüksel 220610036
+Aysha Mallamahmoud 230610013
+Hazar Dardağan 220610010
+Feyza Tiryaki 230610052
+*/
+
 package main;
 
 import entity.PlayerDummy;
--- game/src/main/GamePanel.java
+++ game/src/main/GamePanel.java
@@ -1,4 +1,12 @@
+/*Ali Buğra Tekin 230610058
+Aylin Yüksel 220610036
+Aysha Mallamahmoud 230610013
+Hazar Dardağan 220610010
+Feyza Tiryaki 230610052
+*/
+
 package main;
+
 import ai.PathFinder;
 import data.SaveLoad;
 import entity.Entity;
